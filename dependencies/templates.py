55bdd4b35eaac8fc98ed427824bd649437862d29
Yifan <yifan@YifandeMacBook-Air.local>
2024-09-25 16:53:08 +0800

    新增资讯打标接口

--- dependencies/templates.py
+++ dependencies/templates.py
@@ -35,3 +35,20 @@ article_title_rewrite_template = PromptTemplate.from_template('''
     Note that the output should not contain any Chinese terms such as ‘雨果’ or ‘雨果跨境’. Only output the new title.
     The article title is : {article_title}
 ''')
+
+article_text_rewrite_template = PromptTemplate.from_template('''
+    As a professional consultant in business and trade, please rewrite the following article using detailed and comprehensive statements to provide a better, more readable, and easy-to-understand reading experience.
+    Note: Do not include any Chinese terms such as ‘雨果’ or ‘雨果跨境’ in the output.
+    The content should retain detailed information and avoid being overly summarized or abstract, and the output language should be Chinese.
+    The output should be in HTML format, and only include the rewritten article content, without any additional statements.
+    The output should not have a title, and all HTML output do not contain extra newline characters.
+    The article is : {article_text}
+''')
+
+# 标签清单参考：https://alidocs.dingtalk.com/i/nodes/Amq4vjg89ndMdyQjHLyln5vjW3kdP0wQ
+article_text_tag_template = PromptTemplate.from_template('''
+    Please tag the following article based on its content. The tag list is as follows. Output no more than 3 tags, separated by space, and do not include any additional content. 
+    Strictly maintain the original form of the tags, do not translate them. Duplicate tags should be removed from the output.
+    The tag list is : 亚马逊、ebay、TEMU、TikTok、沃尔玛、家乐福、SHEIN、Shoppe、Lazada、速卖通、美客多、独立站、开店、选品、流量、物流、招商信息、政策法规、美国、加拿大、日韩、东南亚、南美、欧洲、中东、俄罗斯
+    The article is : {article_text}
+''')
