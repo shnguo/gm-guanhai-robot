36fb9e01e46463188f23bf8f6ae438da128a1372
Yifan <yifan@YifandeMacBook-Air.local>
2024-10-09 19:32:35 +0800

    增加中文prompt

--- dependencies/templates.py
+++ dependencies/templates.py
@@ -36,14 +36,25 @@ article_title_rewrite_template = PromptTemplate.from_template('''
     The article title is : {article_title}
 ''')
 
+
+# article_text_rewrite_template = PromptTemplate.from_template('''
+#     As a professional consultant in business and trade, please rewrite the following article using detailed and comprehensive statements to provide a better, more readable, and easy-to-understand reading experience.
+#     Note: Do not include any Chinese terms such as ‘雨果’ or ‘雨果跨境’ in the output.
+#     The content should retain detailed information and to appropriately conclude the article and highlight the key points.
+#     Besides, the output language should be Chinese.
+#     The output should be in HTML format, and only include the rewritten article content, without any additional statements.
+#     The output should not have a title, and all HTML output do not contain extra newline characters.
+#     The article is : {article_text}
+# ''')
 article_text_rewrite_template = PromptTemplate.from_template('''
-    As a professional consultant in business and trade, please rewrite the following article using detailed and comprehensive statements to provide a better, more readable, and easy-to-understand reading experience.
-    Note: Do not include any Chinese terms such as ‘雨果’ or ‘雨果跨境’ in the output.
-    The content should retain detailed information and avoid being overly summarized or abstract, and the output language should be Chinese.
-    The output should be in HTML format, and only include the rewritten article content, without any additional statements.
-    The output should not have a title, and all HTML output do not contain extra newline characters.
-    The article is : {article_text}
+      作为一个商业贸易相关的顾问，请用内容翔实的陈述，改写以下文章正文，提炼出重点并用易读易懂的方式陈述。
+      注意措辞和原文保持一定的差异性。改写后的文章中不要含有‘雨果’ 或 ‘雨果跨境’等词汇。输出内容的格式采用HTML，并且不要含有标题部分和多余的换行符、特殊字符。
+      文章正文：
+      {article_text}
+      注意措辞和原文保持一定的差异性。改写后的文章中不要含有‘雨果’ 或 ‘雨果跨境’等词汇。输出内容的格式采用HTML，并且不要含有标题部分和多余的换行符、特殊字符。
+
 ''')
+
 
 # 标签清单参考：https://alidocs.dingtalk.com/i/nodes/Amq4vjg89ndMdyQjHLyln5vjW3kdP0wQ
 article_text_tag_template = PromptTemplate.from_template('''
@@ -52,3 +63,5 @@ article_text_tag_template = PromptTemplate.from_template('''
     The tag list is : 亚马逊、ebay、TEMU、TikTok、沃尔玛、家乐福、SHEIN、Shoppe、Lazada、速卖通、美客多、独立站、开店、选品、流量、物流、招商信息、政策法规、美国、加拿大、日韩、东南亚、南美、欧洲、中东、俄罗斯
     The article is : {article_text}
 ''')
+
+
