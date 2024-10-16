ef471358166f53971652c1de787bfbf3e86f8836
shnguo <shnguo@gmail.com>
2024-10-16 16:09:39 +0800

    docker rm image

--- build-aigc.sh
+++ build-aigc.sh
@@ -3,3 +3,6 @@ docker buildx build --platform linux/amd64 -t crpi-67yitwjy0o6k9icc.cn-hangzhou.
 docker push crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-aigc:latest
 docker tag crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-aigc:latest crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-aigc:$tag
 docker push crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-aigc:$tag
+
+docker rmi crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-aigc:latest
+docker rmi crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-aigc:$tag
--- build-vit.sh
+++ build-vit.sh
@@ -3,3 +3,6 @@ docker buildx build --platform linux/amd64 -t crpi-67yitwjy0o6k9icc.cn-hangzhou.
 docker push crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-vit:latest
 docker tag crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-vit:latest crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-vit:$tag
 docker push crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-vit:$tag
+
+docker rmi crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-vit:latest
+docker rmi crpi-67yitwjy0o6k9icc.cn-hangzhou.personal.cr.aliyuncs.com/gm-aigc/gm-guanhai-vit:$tag
