861eed63374fd1d7c955a4bb89e4f26fec564fd5
yifanli <iamyifanli@gmail.com>
2024-11-06 20:58:14 +0800

    update the openai api env

--- dependencies/__init__.py
+++ dependencies/__init__.py
@@ -4,22 +4,22 @@ model_map = {
     "gpt4o": AzureChatOpenAI(
         openai_api_key=os.getenv("VEVORPOC_OPENAI_API_KEY"),
         azure_endpoint=os.getenv("VEVORPOC_OPENAI_ENDPOINT"),
-        openai_api_version="2024-03-01-preview",
+        openai_api_version="2024-09-01-preview",
         azure_deployment="gpt-4o",
         temperature=0,
     ),
     "gpt4omini": AzureChatOpenAI(
         openai_api_key=os.getenv("VEVORPOC_OPENAI_API_KEY"),
         azure_endpoint=os.getenv("VEVORPOC_OPENAI_ENDPOINT"),
-        openai_api_version="2024-03-01-preview",
+        openai_api_version="2024-09-01-preview",
         azure_deployment="gpt-4o-mini",
         temperature=0,
     ),
     "gpt-4-turbo-128k": AzureChatOpenAI(
         openai_api_key=os.getenv("SWEDEN_AZURE_OPENAI_API_KEY"),
         azure_endpoint=os.getenv("SWEDEN_AZURE_OPENAI_ENDPOINT"),
-        openai_api_version="2024-03-01-preview",
-        azure_deployment="gpt-4-turbo-128k",
+        openai_api_version="2024-09-01-preview",
+        azure_deployment="gpt-4",
         temperature=0,
     ),
 }
